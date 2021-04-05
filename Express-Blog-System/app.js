59e6cca4072cdf100944cc1b5ba0f739c18f5fb0
George <angelov2000@abv.bg>
2021-04-05 11:57:36 +0300

    blog-system

--- Express-Blog-System/app.js
+++ Express-Blog-System/app.js
@@ -1,37 +1,53 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const db = require("monk")("localhost/nodeblog");
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 const expressValidator = require('express-validator');
+
+const mongo = require('mongodb');
+const db = require('monk')('localhost/nodeblog');
+
+const routes = require('./routes/index');
+const posts = require('./routes/posts');
+const categories = require('./routes/categories');
 
 const app = express();
 
-app.locals.moment = require("moment");
+app.locals.moment = require('moment');
+
+app.locals.truncateText = function(text, length){
+  const truncatedText = text.substring(0, length);
+  return truncatedText;
+}
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Express Session
-app.use(
-  session({
-    secret: "secret",
+app.use(session({
+    secret: 'secret',
     saveUninitialized: true,
-    resave: true,
-  })
-);
+    resave: true
+}));
 
 // Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
+      const namespace = param.split('.')
       , root    = namespace.shift()
       , formParam = root;
 
@@ -48,25 +64,22 @@ app.use(expressValidator({
 
 
 // Connect-Flash
-app.use(require("connect-flash")());
+app.use(require('connect-flash')());
 app.use(function (req, res, next) {
-  res.locals.messages = require("express-messages")(req, res);
+  res.locals.messages = require('express-messages')(req, res);
   next();
 });
 
-app.use(function (req, res, next) {
-  req.db = db;
-  next();
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
 });
 
-//Routes
-const index = require("./routes/index");
-const posts = require("./routes/posts");
+app.use('/', routes);
+app.use('/posts', posts);
+app.use('/categories', categories);
 
-app.use("/", index);
-app.use("/posts", posts);
+app.listen(3000)
 
-// Start Server
-app.listen(3000, function () {
-  console.log("Server started on port 3000...");
-});
+module.exports = app;
--- Express-Blog-System/routes/posts.js
+++ Express-Blog-System/routes/posts.js
@@ -5,6 +5,16 @@ const upload = multer({ dest: './public/images' })
 const mongo = require('mongodb');
 const db = require('monk')('localhost/nodeblog');
 
+router.get('/show/:id', function(req, res, next) {
+	const posts = db.get('posts');
+
+	posts.findById(req.params.id,function(err, post){
+		res.render('show',{
+  			'post': post
+  		});
+	});
+});
+
 router.get('/add', function(req, res, next) {
 	const categories = db.get('categories');
 
@@ -23,6 +33,13 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
   const body = req.body.body;
   const author = req.body.author;
   const date = new Date();
+
+  // Check Image Upload
+  if(req.file){
+  	const mainimage = req.file.filename
+  } else {
+  	const mainimage = 'noimage.jpg';
+  }
 
   	// Form Validation
 	req.checkBody('title','Title field is required').notEmpty();
@@ -51,6 +68,60 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
 				req.flash('success','Post Added');
 				res.location('/');
 				res.redirect('/');
+			}
+		});
+	}
+});
+
+
+router.post('/addcomment', function(req, res, next) {
+  // Get Form Values
+  const name = req.body.name;
+  const email= req.body.email;
+  const body = req.body.body;
+  const postid= req.body.postid;
+  const commentdate = new Date();
+
+  	// Form Validation
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('email','Email field is required but never displayed').notEmpty();
+	req.checkBody('email','Email is not formatted properly').isEmail();
+	req.checkBody('body', 'Body field is required').notEmpty();
+
+	// Check Errors
+	const errors = req.validationErrors();
+
+	if(errors){
+		const posts = db.get('posts');
+		posts.findById(postid, function(err, post){
+			res.render('show',{
+				"errors": errors,
+				"post": post
+			});
+		});
+	} else {
+		const comment = {
+			"name": name,
+			"email": email,
+			"body": body,
+			"commentdate": commentdate
+		}
+
+		const posts = db.get('posts');
+
+		posts.update({
+			"_id": postid
+		},{
+			$push:{
+				"comments": comment
+			}
+		}, function(err, doc){
+			if(err){
+				throw err;
+			} else {
+				req.flash('success', 'Comment Added');
+				res.location('/posts/show/'+postid);
+				res.redirect('/posts/show/'+postid);
 			}
 		});
 	}
